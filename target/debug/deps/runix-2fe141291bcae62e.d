e7470b7bd7fe60d23dae33406f4b26864b5738b3
KiritoEM <johankirito64@gmail.com>
2025-03-08 20:41:48 +0300

    first-commit

--- target/debug/deps/runix-2fe141291bcae62e.d
+++
@@ -1,5 +0,0 @@
-/home/kirito/Desktop/kirito-dev/runix/target/debug/deps/librunix-2fe141291bcae62e.rmeta: src/main.rs
-
-/home/kirito/Desktop/kirito-dev/runix/target/debug/deps/runix-2fe141291bcae62e.d: src/main.rs
-
-src/main.rs:
--- target/debug/deps/runix-d6748060f73b135b.d
+++
@@ -1,5 +0,0 @@
-/home/kirito/Desktop/kirito-dev/runix/target/debug/deps/librunix-d6748060f73b135b.rmeta: src/main.rs
-
-/home/kirito/Desktop/kirito-dev/runix/target/debug/deps/runix-d6748060f73b135b.d: src/main.rs
-
-src/main.rs:
