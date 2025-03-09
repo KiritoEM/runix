2cd3955f0e098e9dd97cdac691dad9f1bb37d7cb
KiritoEM <johankirito64@gmail.com>
2025-03-09 16:50:40 +0300

    feat: add cli

---
+++ src/cli.rs
@@ -0,0 +1,20 @@
+use clap::{arg, Args,  Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author = "KiritoEM", version ="0.0.1", about="\nRunix is a simple compilater")]
+pub struct Cli {
+    #[clap(subcommand)]
+    pub command: Option<Commands>
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    #[command(about="Scan a file")]
+    Scan(Scan)
+}
+
+#[derive(Args, Debug)]
+pub struct Scan {
+    #[arg(short='f', long="file", help="The path of file to scan")]
+    pub file: std::path::PathBuf
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::{CommandFactory, Parser};
+use runix::{Cli, Commands};
+
 fn main() {
-    println!("Hello, world!");
+   let args = Cli::parse();
+
+   match &args.command {
+    Some(Commands::Scan(arg)) => {
+        println!("{:?}", arg.file);
+    },
+    None => {
+        Cli::command().print_help().unwrap();
+    }
+   }
 }
